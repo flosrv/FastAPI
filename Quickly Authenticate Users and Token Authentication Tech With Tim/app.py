271478803a7005ec4cc1e9e20cf3cca7be333cb9
flosrv <flosrv123@gmail.com>
2025-03-30 01:37:16 +0100

    Started building my first account manager

--- Quickly Authenticate Users and Token Authentication Tech With Tim/app.py
+++ Quickly Authenticate Users and Token Authentication Tech With Tim/app.py
@@ -1,105 +1,30 @@
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
-from datetime import datetime, timedelta
+from sqlalchemy.orm import Session
+from models import User as UserModel
+from schemas import User as UserSchema, UserCreate
+from database import get_db
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 from typing import Optional
+import services
 
+app =FastAPI()
 
-SECRET_KEY="ffb723b2f570c1b503dc6374e34373eda3b6999e25dab50139415e211ee05e1c"
+@app.post("signup", status_code=status.HTTP_201_CREATED)
+async def create_user(user:UserCreate,db: Session = Depends(get_db) ):
 
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRES_MINUTES = 30
-
-fake_db= {
-    "username":"flo",
-    "email": "flo@gmail.com",
-     "hashed_pasword": "",
-     "disabled":False
-}
-
-
-class Token(BaseModel):
-    access_token:str
-    token_type:str
-
-
-
-
-class TokenData(BaseModel): 
-    username: Optional[str] = None
-
-class User(BaseModel):
-    username: str
-    email: Optional[str] = None
-    full_name: Optional[str] = None
-    disable: Optional[bool] = None
-
-
-class UserInDB(User):
-    hashed_password:str
-
-class Data(BaseModel):
-    name:str
-
-pwd_context = CryptContext(schemes=["bcrypt"], 
-                           deprecated="auto")
-
-oauth_2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-# "hello" -> "rnerkgegtrhntrkjlhthr"
-
-def verify_pwd(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-def get_pwd_hash(password):
-    return pwd_context.hash(password)
-
-def get_user(db, username : str):
-    if username in db:
-        user_data = db[username]
-        return UserInDB(**user_data)
-
-def authenticate_user(db, username:str, password:str):
-    user = get_user(db, username)
-    if not user:
-        return False
-    if not verify_pwd(password,user.hashed_password) :
-        return False
-    return user
-
-def create_access_token(data:dict, expires_delta: Optional[timedelta]=None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.now(timezone.utc) + expires_delta
-    else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=50)
+    db_user = services.existing_user(db, user.username, user.email)
+    if db_user:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, 
+                            detail="username or email already exists")
     
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-
-
-
-
-
-
-
-
-
-
-app = FastAPI()
-@app.get("/{item_id}/")
-async def test(item_id : str, query : int = 1):
-    return {'hello': item_id}
-
-
-@app.post("/create")
-async def create(data : Data):
-    return {"data" : data}
-
-
+    await services.create_user(db,user)
+    access_token = await services.create_access_token(db_user.id, db_user.username)
+    return {"access_token": access_token,
+            "token_type": "bearer",
+            "username": db_user.username,
+            }    
 
